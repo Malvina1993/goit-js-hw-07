dda7b00797effa8450d8bcdcda0433900ebe2306
Malvina1993 <mykhamalvina@gmail.com>
2023-07-20 22:50:13 +0300

    example 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,36 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const ulEl = document.querySelector('ul.gallery');
+
+galleryToList(galleryItems);
+
+ulEl.addEventListener('click', onImageClick);
+
+
+
+function onImageClick(evn) {
+    
+    if (evn.target.nodeName !== 'IMG') {
+        return;
+    };
+    evn.preventDefault();
+    const instance = basicLightbox.create(`<img src="${evn.target.dataset.source}" width="800" height="600">`);
+    
+    instance.show(); 
+    
+};
+
+function galleryToList(gallery) {
+    ulEl.innerHTML = gallery
+        .map(({ preview, original, description }) => `
+            <li class = "gallery__item">
+                <a class = "gallery__link" href = "${original}">
+                    <img class = "gallery__image" src = "${preview}" alt = "${description}" data-source = "${original}">
+                </a>
+            </li>
+            `)
+        .join("");
+ 
+}
+
