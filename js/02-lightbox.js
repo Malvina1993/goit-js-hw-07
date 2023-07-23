732f7d2bac52be7a22d8127275c9fd07f383a6eb
Malvina1993 <mykhamalvina@gmail.com>
2023-07-23 17:56:05 +0300

    added 2 examle

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,45 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const ulEl = document.querySelector('ul.gallery');
+
+galleryToList(galleryItems);
+
+ulEl.addEventListener('click', onImageClick);
+
+let gallery = new SimpleLightbox('.gallery a', {
+    captionsData: "alt",
+    sourceAttr: "href",
+    captionPosition: "bottom",
+    captionDelay: 250,
+
+});
+
+
+
+function onImageClick(evn) {
+    
+    if (evn.target.nodeName !== 'IMG') {
+        return;
+    };
+    evn.preventDefault();
+    
+
+    gallery.sourceAttr = evn.target.parentNode.getAttribute("href");
+   
+};
+
+function galleryToList(gallery) {
+    ulEl.innerHTML = gallery
+        .map(({ preview, original, description }) => `
+            <li class = "gallery__item">
+                <a class = "gallery__link" href = "${original}">
+                    <img class = "gallery__image" src = "${preview}" alt = "${description}" >
+                </a>
+            </li>
+            `)
+        .join("");
+ 
+};
+
+
